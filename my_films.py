9ab229cc182b205794df62168a064d5fa3babbeb
izumizumoff <izumizumoff@yandex.ru>
2020-09-02 23:24:23 +0500

    module doc description on EN

--- my_films.py
+++ my_films.py
@@ -1,28 +1,27 @@
 '''
-Модуль посвящен моим любимым филмам и сериалам.
-Постоянно обновляемый список фильмов.
-JSON файл 'films' содержит информацию о фильмах:
-	1. Название
-	2. Режиссер
-	3. Год
-	4. Рейтинг
-	 
-Мой личный рейтинг:
-	 
-  5: Ничего особенного, обычный
-  6: Любопытно было посмотреть
-  7: Хороший и интересный, рекомендую
-  8: Крутой фильм! Обязательно к просмотру!
-  9: Один из лучших фильмов, который я видел
-  10: Моё кинопотрясение
-  
- Что может:
- 	1. Добавлять новые фильмы и сохранять их в JSON файле
- 	2. Искать фильмы по имени режиссера: образует список фильмов снятые этим режиссером
- 	3. Искать фильмы по году: образует список фильмов снятые в этом году
- 	4. Искать фильмы по первой букве: образует список по первой букве
- 	5. Список всех режиссеров
- 	6. Список всех фильмов 
+The module is dedicated to my favorite films and TV shows.
+Constantly updated list of films.
+JSON file 'films' contains information about films:
+	1. Title
+	2. Director
+	3. Year
+	4. Rating
+
+My personal rating:
+	5: Nothing special, normal
+	6: It was interesting to see
+	7: Nice and interesting, I recommend
+	8: Cool movie! Must-see!
+	9: One of the Best Movies I've Seen
+	10: My Movie Shake
+
+What can:
+ 	1. Add new movies and save them in JSON file
+	2. Search for films by director's name: generates a list of films made by this director
+	3. Search for films by year: generates a list of films made this year
+	4. Search movies by first letter: makes a list by first letter
+	5. List of all directors
+	6. List of all films
 '''
 class MyFilms:
 	
